2ad510557055ea1660a7d2eb9734965260531b11
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-09-22 03:35:38 +0000

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -3,13 +3,8 @@ def rescue_princess_peach
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
-
-
-
-## Comment the below back in to see method scope in action!
-
-# rescue_princess_peach
-# puts status
+#rescue_princess_peach
+  #puts status
 
 
 
