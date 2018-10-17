20a1518dd680d74941fa83a3a8b252a00a809c87
Corina Talavera <corina.talavera@gmail.com>
2018-10-17 00:28:37 +0000

    Done.

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -5,4 +5,7 @@ describe "#catch_phrase" do
     phrase = "It's-a me, Mario!"
     expect{catch_phrase}.to output("It's-a me, Mario!\n").to_stdout
   end
+  def catch_phrase
+    phrase= "It's-a me, Mario!"
+    puts #phrase
 end
