9f5d885a3c8af9f94e4c1034f6059f468e31a66c
blanca bell <b.p.bell15@gmail.com>
2018-12-12 02:20:14 +0000

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,6 +1,6 @@
 def rescue_princess_peach
   status = "rescued"
-  puts "Hooray! Mario has rescued Princess Peach."
+  puts "Hooray! Mario has #{status} Princess Peach."
 end
 
 
@@ -10,7 +10,3 @@ end
 
 # rescue_princess_peach
 # puts status
-
-
-
-
