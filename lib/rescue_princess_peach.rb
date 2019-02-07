97fa6c7cdf48824f7a8904fcba73e0ffa7e05fdc
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-07 03:10:12 +0000

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,15 +1,11 @@
 def rescue_princess_peach
-  status = "rescued"
+  status = 
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
 
 
 
-## Comment the below back in to see method scope in action!
-
-# rescue_princess_peach
-# puts status
 
 
 
