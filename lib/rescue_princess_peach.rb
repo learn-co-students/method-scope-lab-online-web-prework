fae17a5ee6a5f4648f9ae5d9632173758920f056
David Stewart <davidstewart@gmail.com>
2019-02-10 04:52:47 +0000

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -6,10 +6,12 @@ end
 
 
 
-## Comment the below back in to see method scope in action!
 
-# rescue_princess_peach
-# puts status
+##Comment the below back in to see method scope in action!
+
+####
+#rescue_princess_peach
+#puts status
 
 
 
