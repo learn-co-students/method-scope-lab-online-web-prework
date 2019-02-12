3664b086d38507422ccd442ec3c198f136b7a713
James Phelan <JamesP5444@gmail.com>
2019-02-11 20:02:07 -0500

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,16 +1,13 @@
 def rescue_princess_peach
   status = "rescued"
+  puts status
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
 
+#
 
+# Comment the below back in to see method scope in action!
 
-## Comment the below back in to see method scope in action!
-
-# rescue_princess_peach
-# puts status
-
-
-
-
+rescue_princess_peach
+puts status
